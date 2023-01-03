7b77fd01f8f42905bd907a52a45161a17b10dfcc
bex-ford <becky782@hotmail.co.uk>
2023-01-03 12:26:32 +0000

    added prompt messages

--- starter/script.js
+++ starter/script.js
@@ -87,21 +87,60 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
+var useLowerCase=false
+
+const random = {
+  symbol: specialCharacters,
+  number: numericCharacters,
+  lower: lowerCasedCharacters,
+  upper: upperCasedCharacters
+
+};
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var specialCharacters = prompt("Have you used special characters in your password?");
+  var numericCharacters = prompt("Have you used numeric characters in your password?");
+  var lowerCasedCharacters = prompt("Have you used lower cased characters in your password?");
+  var upperCasedCharacters = prompt("Have you used upper cased characters in your password?");
+
+// Alert the results
+alert(specialCharacters);
+alert(numericCharacters);
+alert(lowerCasedCharacters);
+alert(upperCasedCharacters);
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  return specialCharacters(Math.floor(Math.random()));
 }
+
+function getRandom(arr) {
+  return numericCharacters(Math.floor(Math.random()));
+}
+
+function getRandom(arr) {
+  return lowerCasedCharacters(Math.floor(Math.random()));
+}
+
+function getRandom(arr) {
+  return upperCasedCharacters(Math.floor(Math.random()));
+}
+
+
+// console.log(numericCharacters)
 
 // Function to generate password with user input
 function generatePassword() {
-
+ var password = "";
+ getPasswordOptions() 
+  return password;
 }
+
+//variables scope
+///Users/bex/Downloads/CodingBootcamp/UK-VIRT-FE-PT-11-2022-U-LOLC/week-5-advanced-javascript-module/02-functions-lesson/activities/06-Ins-Scope/script.js
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -116,3 +155,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+// * Generate a password when the button is clicked
+//   * Present a series of prompts for password criteria
+//     * Length of password
+//       * At least 10 characters but no more than 64.
+//     * Character types
+//       * Lowercase
+//       * Uppercase
+//       * Numeric
+//       * Special characters ($@%&*, etc)
+//   * Code should validate for each input and at least one character type should be selected
+//   * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
